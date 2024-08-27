20dca8625442d40ad93370d40093c825f6916934
findreamer <findream@126.com>
2024-08-27 13:01:18 +0800

    feat: 增加读取配置类

---
+++ easyJava/src/main/java/com/easyjava/builder/RunApplication.java
@@ -0,0 +1,8 @@
+package com.easyjava.builder;
+import com.easyjava.builder.Buildtable;
+
+public class RunApplication {
+    public static void main(String[] args) {
+        Buildtable.getTables();
+    }
+}
