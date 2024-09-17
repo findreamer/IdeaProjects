df9a3f05fbb6b61cda779ff6a9f7be549313d074
findreamer <findream@126.com>
2024-09-17 17:11:53 +0800

    feat: 完成类的注解

--- easyJava-demo/src/main/java/com/easyjava/entity/po/ProductInfo.java
+++ easyJava-demo/src/main/java/com/easyjava/entity/po/ProductInfo.java
@@ -1,7 +1,35 @@
 package com.easyjava.entity.po;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.math.BigDecimal;
 
+/**
+ * @Description: 产品信息表
+ * @Date: 时间
+ */
 public class ProductInfo implements Serializable {
+
+  private Integer id;
+
+  private String companyId;
+
+  private String code;
+
+  private String productName;
+
+  private BigDecimal proce;
+
+  private Integer skuType;
+
+  private Integer colorType;
+
+  private Date createTime;
+
+  private Date createDate;
+
+  private Long stock;
+
+  private Integer status;
 
 }
