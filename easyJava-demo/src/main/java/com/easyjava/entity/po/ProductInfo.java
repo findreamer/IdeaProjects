8e245c8bda79163e7300c359182bf0388904fd41
findreamer <findream@126.com>
2024-09-18 22:52:07 +0800

    feat: 完成日期字段的注解

--- easyJava-demo/src/main/java/com/easyjava/entity/po/ProductInfo.java
+++ easyJava-demo/src/main/java/com/easyjava/entity/po/ProductInfo.java
@@ -2,68 +2,74 @@ package com.easyjava.entity.po;
 
 import java.io.Serializable;
 import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
 import java.math.BigDecimal;
 
 /**
  * @Description: 产品信息表
+ *
  * @auther: 钱多多
- * @date: 2024/09/17
+ * @date: 2024/09/18
  */
 public class ProductInfo implements Serializable {
+	/**
+	 * 自增ID
+	 */
+	private Integer id;
 
-  /**
-   * 自增ID
-   */
-  private Integer id;
+	/**
+	 * 公司ID
+	 */
+	private String companyId;
 
-  /**
-   * 公司ID
-   */
-  private String companyId;
+	/**
+	 * 商品编号
+	 */
+	private String code;
 
-  /**
-   * 商品编号
-   */
-  private String code;
+	/**
+	 * 商品名称
+	 */
+	private String productName;
 
-  /**
-   * 商品名称
-   */
-  private String productName;
+	/**
+	 * 价格
+	 */
+	private BigDecimal proce;
 
-  /**
-   * 价格
-   */
-  private BigDecimal proce;
+	/**
+	 * sku类型
+	 */
+	private Integer skuType;
 
-  /**
-   * sku类型
-   */
-  private Integer skuType;
+	/**
+	 * 颜色类型
+	 */
+	private Integer colorType;
 
-  /**
-   * 颜色类型
-   */
-  private Integer colorType;
+	/**
+	 * 创建时间
+	 */
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date createTime;
 
-  /**
-   * 创建时间
-   */
-  private Date createTime;
+	/**
+	 * 创建日期
+	 */
+	@JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private Date createDate;
 
-  /**
-   * 创建日期
-   */
-  private Date createDate;
+	/**
+	 * 库存
+	 */
+	private Long stock;
 
-  /**
-   * 库存
-   */
-  private Long stock;
-
-  /**
-   * 状态
-   */
-  private Integer status;
+	/**
+	 * 状态
+	 */
+	private Integer status;
 
 }
